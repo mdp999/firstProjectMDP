4521646b66d7189de8682711e0eaa2c8ebc28b78
Mikołaj <mikolajolszowiec@gmail.com>
2018-11-18 22:52:39 +0100

    add some hints in ItemService and ItemController

---
+++ src/main/java/com/mdp/ourfirstproject/controller/ItemController.java
@@ -0,0 +1,93 @@
+package com.mdp.ourfirstproject.controller;
+
+import com.mdp.ourfirstproject.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.math.BigDecimal;
+
+@Controller
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "Hello world";
+    }
+
+    @GetMapping(value = "/item/create")
+    public String createItemGet()
+    {
+        //:TODO: wczytanie szablonu w którym można dodać nową transakcję
+        throw new NotImplementedException();
+        //return "Hello world";
+    }
+
+    @PostMapping(value = "/item/create")
+    public String createItemPost(@RequestParam Long productId, @RequestParam BigDecimal price, @RequestParam BigDecimal amount)
+    {
+        itemService.create(productId, price, amount);
+        throw new NotImplementedException();
+//        return "Hello world";
+    }
+
+    @GetMapping(value = "/item/read/id/{id}/")
+    public String readItemByIdGet(@RequestParam Long id)
+    {
+        itemService.readByID(id);
+        throw new NotImplementedException();
+//        return "Hello world";
+    }
+
+    @GetMapping(value = "/item/read/name/{name}/")
+    public String readItemByNameGet(@RequestParam String productName)
+    {
+        itemService.readByName(productName);
+        throw new NotImplementedException();
+//        return "Hello world";
+    }
+
+    @GetMapping(value = "/item/read/keyword/{keyword}/")
+    public String readItemByKeywordGet(@RequestParam String keyword)
+    {
+        itemService.readByKeywordInDescription(keyword);
+        throw new NotImplementedException();
+//        return "Hello world";
+    }
+
+    @GetMapping(value = "/item/readall/")
+    public String readItemByKeywordGet()
+    {
+        itemService.readAll();
+        throw new NotImplementedException();
+//        return "Hello world";
+    }
+
+    @GetMapping(value = "/item/update")
+    public String updateItemGet()
+    {
+        //:TODO: wczytanie szablonu w którym można dodać edytować transakcję
+        throw new NotImplementedException();
+        //return "Hello world";
+    }
+
+    @PostMapping(value = "/item/update")
+    public String updateItemPost(@RequestParam Long itemId, @RequestParam Long productId, @RequestParam BigDecimal price, @RequestParam BigDecimal amount)
+    {
+        itemService.update(itemId, productId, price, amount);
+        throw new NotImplementedException();
+//        return "Hello world";
+    }
+
+    @DeleteMapping(value = "/item/delete/{id}")
+    public String deleteItem(@PathVariable("id") long id) {
+        itemService.delete(id);
+        return "";
+    }
+
+}
--- src/main/java/com/mdp/ourfirstproject/service/ProductService.java
+++ src/main/java/com/mdp/ourfirstproject/service/ProductService.java
@@ -1,32 +1,31 @@
 package com.mdp.ourfirstproject.service;
 
 import com.mdp.ourfirstproject.Exceptions.NoSuchProductException;
+import com.mdp.ourfirstproject.model.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class ProductService {
 
+@Service
+public class ProductService {
 
     
     public void create(Product product) {
 
     }
-
     
     public Product readByID(long id) throws NoSuchProductException {
         return null;
     }
-
     
     public void update(Product product) {
 
     }
-
     
     public void delete(long id) {
 
     }
-
     
     public List<Product> readByName(String productName) {
         return null;
