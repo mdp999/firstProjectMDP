3888d7167d1af683d129ee85bddc5162b9452f88
Mikołaj <mikolajolszowiec@gmail.com>
2018-11-18 11:35:14 +0100

    first commit

---
+++ src/main/java/com/mdp/ourfirstproject/OurfirstprojectApplication.java
@@ -0,0 +1,12 @@
+package com.mdp.ourfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurfirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurfirstprojectApplication.class, args);
+	}
+}
