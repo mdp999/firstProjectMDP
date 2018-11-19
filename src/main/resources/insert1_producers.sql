bb17539dd557f34f00b72d0f0160edf43c24d85a
Dominik Bodzioch <do_mi_no@wp.pl>
2018-11-19 21:32:37 +0100

    insert1_producers.sql + insert2_products.sql

---
+++ src/main/resources/insert1_producers.sql
@@ -0,0 +1,7 @@
+INSERT INTO financemanager.producers (id_producer, description, name, producer_id) VALUES (1, null, 'Piątnica', null);
+INSERT INTO financemanager.producers (id_producer, description, name, producer_id) VALUES (2, null, 'Mlekpol', null);
+INSERT INTO financemanager.producers (id_producer, description, name, producer_id) VALUES (3, null, 'Krasnystaw', null);
+INSERT INTO financemanager.producers (id_producer, description, name, producer_id) VALUES (4, null, 'Zott', null);
+INSERT INTO financemanager.producers (id_producer, description, name, producer_id) VALUES (5, null, 'Sobik', null);
+INSERT INTO financemanager.producers (id_producer, description, name, producer_id) VALUES (6, null, 'Pasłęk', null);
+INSERT INTO financemanager.producers (id_producer, description, name, producer_id) VALUES (7, null, ' ', null);
